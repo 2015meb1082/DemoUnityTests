d5ef457ec4ddda431ac3a8cac37d8feaa52ad9d1
2015meb1082 <abhishek.sainiss441998@gmail.com>
2020-10-03 15:31:37 +0530

    First commit with coroutine and raycasting code

---
+++ Assets/Test1/Scripts/RaycastTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RaycastTest : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask mask;
+    private GameObject hitGameObject;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position,transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 100, mask,QueryTriggerInteraction.Ignore))
+        {
+            hitGameObject = hitInfo.collider.gameObject;
+            hitGameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+        }
+        else {
+            if (hitGameObject) {
+                hitGameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+            }
+            Debug.DrawLine(ray.origin, ray.direction * 100, Color.green);
+        }
+    }
+}
---
+++ Assets/Test3/Scripts/TestCoroutines.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TestCoroutines : MonoBehaviour
+{
+    IEnumerator currentCoroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] messages = { "Welcome", "to", "game", "Development. ", "It\'s", "Awesome." };
+        StartCoroutine(MessagePrint(messages,0.5f));
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (currentCoroutine != null) {
+                StopCoroutine(currentCoroutine);
+            }
+
+            currentCoroutine = Move(Random.onUnitSphere * 5, 10);
+            StartCoroutine(currentCoroutine);
+        }
+    }
+    IEnumerator Move(Vector3 destination,float speed) {
+        while (transform.position!=destination) {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+    IEnumerator MessagePrint(string[] messages,float delay) {
+
+        foreach (string message in messages) {
+            print(message);
+            yield return new WaitForSeconds(delay);
+        }
+        
+    }
+}
